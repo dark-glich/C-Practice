16c221013d5889285a485e95e373c419da504326
dark-glich <mohdbilal9808464636@gmail.com>
2021-07-27 10:11:13 +0530

    Completed Variables

--- Main.cc
+++ Main.cc
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    // Declared and Assinged String Variable
+    string characterName = "John";
+    // Declared int variable
+    int characterAge;
+    // Assinged int variable
+    characterAge = 40;
+
+    cout << "There was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old";
     return 0;
 }
