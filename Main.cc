bf374bc3f477b4d88c100cf9e2defb9c19dc7d1d
dark-glich <mohdbilal9808464636@gmail.com>
2021-07-31 10:16:53 +0530

    completed user inputs

--- Main.cc
+++ Main.cc
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     // Declared and Assinged String Variable
     string characterName = "John";
     // Declared int variable
@@ -17,22 +18,30 @@ int main(){
 
     /* Data Types */
     string Name = "Bilal"; // String Variable
-    int Age = 15; // Int Varible
-    double Marks = 94.6; //decimal Variable
-    char Grade = 'A'; //character Variable
-    bool isMale = true; //boolean Variable
+    int Age = 15;          // Int Varible
+    double Marks = 94.6;   //decimal Variable
+    char Grade = 'A';      //character Variable
+    bool isMale = true;    //boolean Variable
 
     /* Sub Strings */
     string sentence = "Apple is red";
-    string sub = sentence.substr(9, 3 ); // string.substr(index, length)
+    string sub = sentence.substr(9, 3); // string.substr(index, length)
     cout << sub << endl;
 
     /* Math Funnctions*/
     int num = 10;
-    cout << pow(num, 2) << endl; // Does 10 raise to the power 2
-    cout << fmax(10, 12) << endl; // Gives the Max number
+    cout << pow(num, 2) << endl;   // Does 10 raise to the power 2
+    cout << fmax(10, 12) << endl;  // Gives the Max number
     cout << ceil(10.4193) << endl; // Round up
-    cout << floor(2.983) << endl; // Round Down
- 
+    cout << floor(2.983) << endl;  // Round Down
+
+    /* User Input */
+    int age;
+    string name;
+    cin >> age; // cin is used to get user input in Char or int
+    cout << "You are " << age << " years old" << endl;
+    getline(cin, name); // getline is used to get user input in string of text
+    cout << "Your name is" << name << endl;
+
     return 0;
 }
