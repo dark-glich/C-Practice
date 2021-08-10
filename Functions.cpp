f299e1fa7e21fdd84fe33875edea49d43a7cbbc9
dark-glich <mohdbilal9808464636@gmail.com>
2021-08-10 10:35:51 +0530

    Completed deafault argument

--- Functions.cpp
+++ Functions.cpp
@@ -10,9 +10,16 @@ void SquareNum(float x){ // with float argument
     cout << x*x << endl;    
 };
 
+// Using default argument as 1
+void Cube(int l=1, int b=1, int h=1){ 
+    cout << "Cube : "<< l*b*h << endl;
+}
+
 int main(){
     int a = 10;
     float b = 3.1428;
     SquareNum(a); // Using SquareNum function using int 
     SquareNum(b); // Using SquareNum function using float
+    Cube(2,5,6);
+    Cube(); // will take l,b,h as 1
 }
