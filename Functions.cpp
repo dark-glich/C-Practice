96083d6682924312540b5d5ddb4a1d53f1244698
dark-glich <mohdbilal9808464636@gmail.com>
2021-08-11 07:33:02 +0530

    Completed Array as arguments

--- Functions.cpp
+++ Functions.cpp
@@ -15,6 +15,13 @@ void Cube(int l=1, int b=1, int h=1){
     cout << "Cube : "<< l*b*h << endl;
 }
 
+// When we pass array as an argument we use '[]'
+void PrintArray(int Array[], int ArraySize){
+    for(int x = 0; x < ArraySize; x++){
+        cout << Array[x] << ", ";
+    }
+};
+
 int main(){
     int a = 10;
     float b = 3.1428;
@@ -22,4 +29,6 @@ int main(){
     SquareNum(b); // Using SquareNum function using float
     Cube(2,5,6);
     Cube(); // will take l,b,h as 1
+    int array[3] =  {10,20,30};
+    PrintArray(array, 3);
 }
