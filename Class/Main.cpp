8f1651cc6327b3c88774aeb7f4f5b7f03c159613
dark-glich <mohdbilal9808464636@gmail.com>
2021-09-11 10:41:36 +0530

    Completed Inheritence

--- Class/Main.cpp
+++ Class/Main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+class Book{
+    public:
+        std::string title;
+        std::string author;
+        double price;
+        int pages;
+
+    protected:
+        int part;
+
+    // Constructor : it is a function that get executed when object is created
+    Book(std::string Atitile, std::string Aauthor, double Aprice, int Apages){
+        title = Atitile;
+        author = Aauthor;
+        price = Aprice;
+        pages = Apages;
+    };
+
+    // Destructor : it is a function that get executed when work of object is complete
+    ~Book(){
+        std::cout << "Object Deleted";
+    }
+};
+
+// Inheritence : A derived class can access all the non-private members of its base class
+class MathsBook: public Book{
+    public:
+        int standard;
+        char edition;
+};
+
 int main(){
-    std::cout << "" << std::endl;
+    Book book1("Harry Potter", "JK Rowling", 199.89, 350);
     return 0;
 }
