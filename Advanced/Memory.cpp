314b230fcb122aa80535a864b5d5cabb1eda3ae8
dark-glich <mohdbilal9808464636@gmail.com>
2021-09-14 09:46:08 +0530

    completed 'new' operator

---
+++ Advanced/Memory.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    // Heap Allocation : This memory is allocated during the execution of instructions written by programmers.
+    // Stack memory : the stack memory is where local variables get stored/constructed.
+
+    // new Operator : new operator is used to allocate memory dynamically for any data-type.
+    double *value = NULL; 
+    value = new double; // allocating memory for a double 
+    *value = 3.1428;
+    std::cout << "Memory : " << value << ", Value : " << *value << '\n';
+    // delete operator : deletes the allocated memory
+    delete value; 
+    return 0;
+} 
